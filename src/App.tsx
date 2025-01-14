b480b0216669b777f5c080395cabdd3338d67bcc
Mimizaya <miapdrsn@gmail.com>
2025-01-14 18:16:37 +0100

    test

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,18 @@
 import './App.css';
+
 export default function App() {
+
+
+  caches.keys().then(function(names) {
+      for (let name of names)
+          caches.delete(name);
+  });
+
   return (
     <div>
-      <img src="./icon.webp" />
+      <img src="./images/icon.webp" />
       <div id="text">
-        <h1>We have moved.
+        <h1>We have moved
         <br/>
         <a href="https://www.crimsonwitch.com"><span className="link-sub">https://www.</span>crimsonwitch.com</a>
         </h1>
--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,5 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import { VitePWA } from 'vite-plugin-pwa';
 
 const ReactCompilerConfig = {
   target: '19' // '17' | '18' | '19'
@@ -16,30 +15,8 @@ export default defineConfig({
         ],
       },
     }),
-    VitePWA({
-      registerType: 'autoUpdate', // Automatically update service worker
-      workbox: {
-        // Specify caching options here
-        runtimeCaching: [
-          {
-            urlPattern: (event: any) => {
-              const { url } = event; // event is a FetchEvent, which has a 'url' property
-              return url.pathname.startsWith('/images/'); // Cache assets from public folder
-            },
-            handler: 'CacheFirst', // Use Cache-First strategy (prefer cache)
-            options: {
-              cacheName: 'assets-cache',
-              expiration: {
-                maxEntries: 1000, // Limit to 1000 images
-                maxAgeSeconds: 60 * 60 * 24 * 30, // Cache for 30 days
-              },
-            },
-          },
-        ],
-      },
-    }),
   ],
-  base: '/', // GitHub repo name
+  base: '/is-this-artifact-good/', // GitHub repo name
   server: {
     host: true, // or use '0.0.0.0'
     port: 5173, // or your preferred port
